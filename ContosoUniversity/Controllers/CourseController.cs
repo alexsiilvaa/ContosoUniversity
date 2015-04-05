f8d132d158e46fedc36a1aed0d9330abb4bd38d8
krzyhook <krzyhook@gmail.com>
2015-04-05 12:02:31 +0200

    This is second commit from VS

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -64,6 +64,7 @@ namespace ContosoUniversity.Controllers
             }
             catch (RetryLimitExceededException)
             {
+               //Some changes for second commit.
                 //Log the error (uncomment dex variable name and add a line here to write a log.)
                 ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
     
